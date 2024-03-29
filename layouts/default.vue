3a407135bef141c79f14d1054191af136b555171
Fran J <francisco.salvador@smartinsurance.es>
2024-03-29 21:13:18 +0100

    feat(Setup): :sparkles: add basic Nuxt setup and required libraries
    Enhance project setup by integrating Prettier and ESLint for code formatting and linting, Tailwind for utility-first CSS, and Headless UI for accessible interface components. This setup establishes a solid foundation for building a user-friendly and developer-friendly interface.

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <div class="container mx-auto sm:px-6 lg:px-8">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
