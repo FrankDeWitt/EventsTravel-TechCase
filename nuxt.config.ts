3522abdd4c8d1fbcfe93962b04caf20cdac4abd8
Fran J <francisco.salvador@smartinsurance.es>
2024-03-30 11:51:08 +0100

    style(Setup): :sparkles: include unstyled components ui library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import * as path from 'path'
+
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', 'nuxt-primevue'],
+  primevue: {
+    options: {
+      unstyled: true,
+    },
+    importPT: { from: path.resolve(__dirname, './presets/lara/') }, //import and apply preset
+  },
+  css: ['~/assets/css/base.css'],
   devtools: { enabled: true },
 })
