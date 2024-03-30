3e865079f22776bf4b10e19b07b2708c2bacde84
Fran J <francisco.salvador@smartinsurance.es>
2024-03-30 01:02:18 +0100

    feat: :sparkles: global components added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,23 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <div class="container mx-auto sm:px-6 lg:px-8">
+  <div class="body-wrapper">
+    <TheHeader />
+    <div class="container mx-auto my-px-6 lg:p-8">
       <slot />
     </div>
+    <TheFooter />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.body-wrapper {
+  display: flex;
+  flex-direction: column;
+  @media screen(lg) {
+    display: grid;
+    grid-template-rows: min-content auto min-content;
+    height: 100vh;
+  }
+}
+</style>
